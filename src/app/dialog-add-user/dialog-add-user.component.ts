439438955e60e80d9264e982a5d34146620bea57
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-26 17:21:07 +0300

    added cancel dial. and dial data transfer on save

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { User } from '../models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -8,7 +9,13 @@ import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angula
 })
 export class DialogAddUserComponent {
 
+  user: User = new User();
+
   constructor() { }
 
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  saveUser() {
+    console.log(this.user);
+  }
 }

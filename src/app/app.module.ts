7bf392e2577f0401fb62f77a3d5103438c019cc9
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-26 11:31:05 +0300

    added component user & dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
